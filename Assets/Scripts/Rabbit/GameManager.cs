f97f3f315317c10a53f58e1c8b21ac4fe5241ec5
federico.cara <federico.cara@etermax.com>
2023-02-05 15:12:20 -0300

    Sonido de pasos

--- Assets/Scripts/Rabbit/GameManager.cs
+++ Assets/Scripts/Rabbit/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance;
     public static GameManager Instance => _instance;
 
+    [SerializeField] private GameObject victoryPanel;
+
     private void Awake()
     {
         _instance = this;
@@ -15,5 +17,6 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Ganaste");
         Time.timeScale = 0;
+        victoryPanel.SetActive(true);
     }
 }
